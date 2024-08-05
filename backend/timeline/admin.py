208d329ab48dd3f48f04fb120d87d41a1ced3cb4
ikechukwueze <themedric@live.com>
2024-08-05 22:41:57 +0100

    add new fields in timeline admin

--- backend/timeline/admin.py
+++ backend/timeline/admin.py
@@ -5,4 +5,13 @@ from .models import TimeLine
 
 @admin.register(TimeLine)
 class TimeLineAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'tweet', 'referenced_tweet', 'tweet_author', 'tweet_type']
+
+    def tweet_author(self, obj):
+        return obj.tweet.author
+    
+    def tweet_type(self, obj):
+        return obj.tweet.tweet_type
+    
+    def referenced_tweet(self, obj):
+        return obj.tweet.referenced_tweet

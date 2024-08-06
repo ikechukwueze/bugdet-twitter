d05a38557bf0d98531aa506931b41308dd3f59d7
ikechukwueze <themedric@live.com>
2024-08-06 07:21:06 +0100

    updated account response object

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -35,6 +35,9 @@ class AccountLoginView(views.APIView):
         account = serializer.validated_data["account"]
         account_details = {
             "email": account.email,
+            "username": account.username,
+            "display_name": account.display_name,
+            "profile_pic": account.profile_pic,
             "token": account.get_token(),
         }
         return response.Response(account_details, status=status.HTTP_200_OK)

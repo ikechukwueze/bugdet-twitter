07dbcb305d2ba085fc94e18b0306ad9bdc8ec25d
ikechukwueze <themedric@live.com>
2024-08-11 22:03:37 +0100

    layout

---
+++ frontend/src/layouts/ThreeColumnLayout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container text-center">
+        <div class="row align-items-start">
+            <div class="col-2">
+                <slot name="left-sidebar"></slot>
+            </div>
+            <div class="col-7 px-5">
+                <slot name="main"></slot>
+            </div>
+            <div class="col-3">
+                <slot name="right-sidebar"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    name: 'ThreeColumnLayout'
+}
+</script>

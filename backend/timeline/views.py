54c3cfb76ad75b4a37a1fcc8be057f83daedb604
ikechukwueze <themedric@live.com>
2024-08-15 23:00:57 +0100

    updates

--- backend/timeline/views.py
+++ backend/timeline/views.py
@@ -1,6 +1,4 @@
-from django.db.models import Count, Q, OuterRef, Subquery, Value
 from rest_framework.generics import ListAPIView
-from tweets.models import TweetType, Tweet
 from .serializers import TimelineSerializer
 from .models import TimeLine
 
@@ -8,16 +6,14 @@ from .models import TimeLine
 # Create your views here.
 
 
-
 class HomeTimelineView(ListAPIView):
     serializer_class = TimelineSerializer
 
-    def get_queryset(self):        
+    def get_queryset(self):
         queryset = (
             TimeLine.objects.select_related("tweet__author", "tweet__referenced_tweet")
             .filter(viewers=self.request.user)
-            .annotate(owner=Value(self.request.user.username))
-            .order_by('-tweet__created_at')
+            .order_by("-tweet__created_at")
         )
         return queryset
 
@@ -27,5 +23,7 @@ class UserTimelineView(ListAPIView):
 
     def get_queryset(self):
         username = self.kwargs.get("username")
-        queryset = TimeLine.objects.filter(tweet__author__username=username)
+        queryset = TimeLine.objects.filter(tweet__author__username=username).order_by(
+            "-created_at"
+        )
         return queryset

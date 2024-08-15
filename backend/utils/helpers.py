720a418d5145750c3c95518dd8e5d3cbdf34a560
ikechukwueze <themedric@live.com>
2024-08-15 22:59:18 +0100

    updated notification helpers

--- backend/utils/helpers.py
+++ backend/utils/helpers.py
@@ -39,20 +39,17 @@ def reformat_tweet_timestamp(timestamp_str: str):
     return datetime.strftime(timestamp, output_format)
 
 
-
 def clean_username(username_token: str):
-    punctuations = ['.', ',', '/', '\\', '!', '?']
+    punctuations = [".", ",", "/", "\\", "!", "?"]
 
     while username_token[-1] in punctuations:
         username_token = username_token[:-1]
-    
+
     return username_token
 
 
 def detect_username_mention(content):
-    username_tokens = [
-        token[1:] for token in content.split() if token.startswith("@")
-    ]
+    username_tokens = [token[1:] for token in content.split() if token.startswith("@")]
     mentions = [clean_username(token) for token in username_tokens]
     return mentions
 
@@ -74,13 +71,18 @@ def push_tweet_to_followers(account: Account, tweet: Tweet):
 
 
 def create_notification(
-    sender: Account, recipient: Account, tweet: Tweet, notification_type
+    sender: Account,
+    recipient: Account,
+    sender_tweet: Tweet,
+    recipient_tweet: Tweet,
+    notification_type,
 ):
     if sender != recipient:
         notification = Notification.objects.create(
             sender=sender,
             recipient=recipient,
-            tweet=tweet,
+            sender_tweet=sender_tweet,
+            recipient_tweet=recipient_tweet,
             notification_type=notification_type,
         )
         return notification
@@ -88,10 +90,11 @@ def create_notification(
 
 def create_follow_notification(sender: Account, recipient: Account):
     notification = create_notification(
-        sender=sender, 
+        sender=sender,
         recipient=recipient,
-        tweet=None, 
-        notification_type=NotificationType.FOLLOW
+        recipient_tweet=None,
+        sender_tweet=None,
+        notification_type=NotificationType.FOLLOW,
     )
     return notification
 
@@ -105,62 +108,74 @@ def create_mention_notification(sender: Account, tweet: Tweet):
             continue
         else:
             create_notification(
-                sender, 
-                recipient, 
-                tweet, 
-                NotificationType.MENTION
+                sender=sender,
+                recipient=recipient,
+                recipient_tweet=None,
+                sender_tweet=tweet,
+                notification_type=NotificationType.MENTION,
             )
 
 
-def create_like_notification(sender: Account, recipient: Account, tweet: Tweet):
+def create_like_notification(sender: Account, recipient: Account, liked_tweet: Tweet):
     notification = create_notification(
-        sender=sender, 
+        sender=sender,
         recipient=recipient,
-        tweet=tweet, 
+        recipient_tweet=liked_tweet,
+        sender_tweet=None,
         notification_type=NotificationType.LIKE,
     )
     return notification
 
 
-def create_dislike_notification(sender: Account, recipient: Account, tweet: Tweet):
+def create_dislike_notification(
+    sender: Account, recipient: Account, disliked_tweet: Tweet
+):
     notification = create_notification(
-        sender=sender, 
-        recipient=recipient, 
-        tweet=tweet,
-        notification_type=NotificationType.DISLIKE
+        sender=sender,
+        recipient=recipient,
+        recipient_tweet=disliked_tweet,
+        sender_tweet=None,
+        notification_type=NotificationType.DISLIKE,
     )
     return notification
 
 
-def create_retweet_notification(sender: Account, recipient: Account, tweet: Tweet):
+def create_retweet_notification(sender: Account, recipient: Account, reposted_tweet: Tweet):
     notification = create_notification(
-        sender=sender, 
+        sender=sender,
         recipient=recipient,
-        tweet=tweet, 
+        recipient_tweet=reposted_tweet,
+        sender_tweet=None,
         notification_type=NotificationType.RETWEET,
     )
     return notification
 
 
-def create_quote_notification(sender: Account, recipient: Account, tweet: Tweet):
+def create_quote_notification(
+    sender: Account, recipient: Account, quoted_tweet: Tweet, quote: Tweet
+):
     notification = create_notification(
-        sender=sender, 
+        sender=sender,
         recipient=recipient,
-        tweet=tweet, 
+        recipient_tweet=quoted_tweet,
+        sender_tweet=quote,
         notification_type=NotificationType.QUOTE,
     )
     return notification
 
 
-def create_reply_notification(sender: Account, recipient: Account, tweet: Tweet):
+def create_reply_notification(
+    sender: Account, recipient: Account, original_tweet: Tweet, reply: Tweet
+):
     notification = create_notification(
-        sender=sender, 
-        recipient=recipient, 
-        tweet=tweet,
-        notification_type=NotificationType.REPLY
+        sender=sender,
+        recipient=recipient,
+        sender_tweet=reply,
+        recipient_tweet=original_tweet,
+        notification_type=NotificationType.REPLY,
     )
     return notification
 
 
-def mark_notification_as_read(notifications_qs:QuerySet):
+def mark_notification_as_read(notifications_qs: QuerySet):
     pass

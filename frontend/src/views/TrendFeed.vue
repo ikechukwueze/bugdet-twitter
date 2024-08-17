84331c25b1b7c625bf55ab93f01067487f36ecf5
ikechukwueze <themedric@live.com>
2024-08-17 18:25:26 +0100

    trend feed vue

---
+++ frontend/src/views/TrendFeed.vue
@@ -0,0 +1,42 @@
+<template>
+  <ThreeColumnLayout>
+    <template #left-sidebar>
+      <MenuVue />
+    </template>
+    <template #main>
+      <SearchBar :placeholder="$route.params.trend_term" />
+      <TrendTweets :trend_term="$route.params.trend_term" />
+    </template>
+    <template #right-sidebar>
+      <TrendList />
+    </template>
+  </ThreeColumnLayout>
+</template>
+
+
+
+<script>
+import ThreeColumnLayout from "@/layouts/ThreeColumnLayout.vue"
+import MenuVue from "@/components/Menu.vue";
+import TrendTweets from "@/components/TrendTweets.vue";
+import SearchBar from "@/components/SearchBar.vue";
+import TrendList from "@/components/TrendList.vue";
+
+
+export default {
+  name: "ThreadVue",
+  components: {
+    ThreeColumnLayout,
+    MenuVue,
+    SearchBar,
+    TrendTweets,
+    TrendList
+  }
+};
+</script>
+
+<style>
+.main {
+  max-width: 1200px;
+}
+</style>

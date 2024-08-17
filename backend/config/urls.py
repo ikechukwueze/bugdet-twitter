905e6a4739ea9d3fdf15e5c8ae9874a87a715b60
ikechukwueze <themedric@live.com>
2024-08-17 12:16:35 +0100

    add app to config

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -45,6 +45,7 @@ urlpatterns = [
     path('followers/', include('followers.urls')),
     path('timeline/', include('timeline.urls')),
     path('notifications/', include('notifications.urls')),
+    path('trends/', include('trends.urls')),
     path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 

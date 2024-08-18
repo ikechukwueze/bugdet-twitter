2bbf7ffad06481ecf1c7723a4ec3a1e7aa6d8a33
ikechukwueze <themedric@live.com>
2024-08-18 16:53:46 +0100

    update ids

--- frontend/src/components/TrendTweets.vue
+++ frontend/src/components/TrendTweets.vue
@@ -2,21 +2,21 @@
     <div class="col-9 mt-4 w-100 text-start">
         <nav>
             <div class="nav nav-pills nav-fill" id="nav-tab" role="tablist">
-                <button class="nav-link active" id="nav-toptweets-tab" data-bs-toggle="tab"
-                    data-bs-target="#nav-toptweets" type="button" role="tab" aria-controls="nav-toptweets"
+                <button class="nav-link active" id="nav-trend-toptweets-tab" data-bs-toggle="tab"
+                    data-bs-target="#nav-trend-toptweets" type="button" role="tab" aria-controls="nav-trend-toptweets"
                     aria-selected="true">Top</button>
 
-                <button class="nav-link" id="nav-latesttweets-tab" data-bs-toggle="tab"
-                    data-bs-target="#nav-latesttweets" type="button" role="tab" aria-controls="nav-latesttweets"
+                <button class="nav-link" id="nav-trend-latesttweets-tab" data-bs-toggle="tab"
+                    data-bs-target="#nav-trend-latesttweets" type="button" role="tab" aria-controls="nav-trend-latesttweets"
                     aria-selected="false">Latest</button>
             </div>
         </nav>
         <div class="tab-content px-3" id="nav-tabContent">
-            <div class="tab-pane fade show active" id="nav-toptweets" role="tabpanel"
-                aria-labelledby="nav-toptweets-tab" tabindex="0">
+            <div class="tab-pane fade show active" id="nav-trend-toptweets" role="tabpanel"
+                aria-labelledby="nav-trend-toptweets-tab" tabindex="0">
                 <TweetList :tweets="top_tweets" />
             </div>
-            <div class="tab-pane fade" id="nav-latesttweets" role="tabpanel" aria-labelledby="nav-latesttweets-tab"
+            <div class="tab-pane fade" id="nav-trend-latesttweets" role="tabpanel" aria-labelledby="nav-trend-latesttweets-tab"
                 tabindex="0">
                 <TweetList :tweets="latest_tweets" />
             </div>
@@ -49,7 +49,7 @@ export default {
         get_trend_top_tweets(trend_term) {
             axios({
                 method: "get",
-                url: `/trends/${trend_term}/top-tweets?limit=100`,
+                url: `search/trends/?search=${trend_term}&ordering=-interactions&limit=100`,
             })
                 .then((response) => {
                     this.top_tweets = response.data.results;
@@ -61,7 +61,7 @@ export default {
         get_trend_latest_tweets(trend_term) {
             axios({
                 method: "get",
-                url: `/trends/${trend_term}/latest-tweets?limit=100`,
+                url: `search/trends/?search=${trend_term}&ordering=-created_at&limit=100`,
             })
                 .then((response) => {
                     this.latest_tweets = response.data.results;

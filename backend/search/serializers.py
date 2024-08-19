33dd69f4e94a14b47164a638342c94bb9388ab62
ikechukwueze <themedric@live.com>
2024-08-19 22:30:15 +0100

    removed cmment

--- backend/search/serializers.py
+++ backend/search/serializers.py
@@ -5,17 +5,9 @@ from tweets.serializers import TweetSerializer
 
 
 class ExtendedTweetSerializer(TweetSerializer):
-    # like_count = serializers.IntegerField(read_only=True)
-    # dislike_count = serializers.IntegerField(read_only=True)
-    # reply_count = serializers.IntegerField(read_only=True)
-    # reference_count = serializers.IntegerField(read_only=True)
     engagement = serializers.IntegerField(read_only=True)
 
     class Meta(TweetSerializer.Meta):
         fields = TweetSerializer.Meta.fields + [
-            # "like_count",
-            # "dislike_count",
-            # "reply_count",
-            # "reference_count",
             "engagement",
         ]
